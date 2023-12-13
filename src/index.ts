5e87d322af93e98cb14db58e057d660a968cea83
Nestor Castañeda <nestor8772@gmail.com>
2023-12-12 21:22:39 -0600

    Se implementa REGEX para analizar el string de req.url

--- src/index.ts
+++ src/index.ts
@@ -11,11 +11,36 @@ const notes = [
     { id: 6, content: "Almorzar a tiempo" },
   ];
 
-const requestListener: http.RequestListener = (_req, response) => {
-   
-    response.write(JSON.stringify(notes));
-    response.end();
-};
- 
-const server = http.createServer(requestListener);
-server.listen(5500);
+
+  const requestListener: http.RequestListener = (req, response) => {
+
+    
+      if (req.url === '/') {
+          console.log("Petición recibida!");
+          response.writeHead(200, { "Content-Type": "text/plain" });
+          response.end("Bienvenido a notas!");
+      }
+      if (req.url === '/notes') {
+        console.log("Petición recibida!");
+        response.writeHead(200, { "Content-Type": "application/json" });
+        response.end(JSON.stringify(notes));  
+    }else {
+      const match = req.url?.match(/\/notes\/(\d+)$/); // agrega guardia de tipo para asegurar que 'req.url' no es undefined
+      if (match) {
+          const id = parseInt(match[1]);
+          const note = notes.find((note) => note.id === id);
+          if (note) {
+              response.writeHead(200, { "Content-Type": "application/json" });
+              response.end(JSON.stringify(note));
+          } else {
+              response.writeHead(404, { "Content-Type": "application/json" });
+              response.end(JSON.stringify({ message: `Note not found` }));
+          }
+      }
+  }
+
+
+  }
+
+  const server = http.createServer(requestListener);
+   server.listen(5500);
