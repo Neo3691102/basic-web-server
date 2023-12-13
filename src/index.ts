f954f5f1bd3cdfecf1f500fd16368d086cbee5f0
Nestor Castañeda <nestor8772@gmail.com>
2023-12-12 18:18:31 -0600

    manipulando el request y el response

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
+import http from "node:http";
+ 
 console.log("Node JS + Typescript")
+
+const notes = [
+    { id: 1, content: "Hacer la cama" },
+    { id: 2, content: "Lavar los platos" },
+    { id: 3, content: "Estudiar JavaScript" },
+    { id: 4, content: "Hacer las compras" },
+    { id: 5, content: "Limpiar mi habitación" },
+    { id: 6, content: "Almorzar a tiempo" },
+  ];
+
+const requestListener: http.RequestListener = (_req, response) => {
+   
+    response.write(JSON.stringify(notes));
+    response.end();
+};
+ 
+const server = http.createServer(requestListener);
+server.listen(5500);
